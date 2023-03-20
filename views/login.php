fa5b40c82432bfae928d527967a7e9482d40fb46
Vianney <vianney.giovannelli.simplon@gmail.com>
2023-03-20 16:06:48 +0100

    add view login + client + template

---
+++ views/login.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" href="./styles/login.css">
+
+
+<div class="container">
+    <div class="brand-logo"></div>
+    <div class="brand-title">Thinkimmo</div>
+    <div class="inputs">
+        <label>EMAIL</label>
+        <input type="email" placeholder="example@test.com" />
+        <label>Mot de passe</label>
+        <input type="password" placeholder="Min 6 charaters long" />
+        <button type="submit">Se connecter</button>
+    </div>
+</div>
