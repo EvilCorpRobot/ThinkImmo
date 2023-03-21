9fe12635c5382f4740f4d07fd6be6b3068ed2c03
Thibaud-M326 <thibaudmaitre@gmail.com>
2023-03-21 15:13:05 +0100

    all routes add files are created

--- controllers/property.php
+++ controllers/property.php
@@ -14,7 +14,8 @@ class ControllerProperty {
     //permet d'afficher la page ClientHome
     //titre - image des appartements et des maisons
     public function displayClientHome() {
-
+        $vue = new View("Login");
+        $vue->generer(["error" => false]);
     }
 
     //permet d'afficher la page ClientHome apres filtrage
