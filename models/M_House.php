74602b6b48de928a3ac6c26b8a14c422b44bec9d
Thibaud-M326 <thibaudmaitre@gmail.com>
2023-03-23 00:33:46 +0100

    addHouse is working, execute params, pictures and 'type' not done

--- models/M_House.php
+++ models/M_House.php
@@ -3,5 +3,15 @@
 require_once 'models/model.php';
 
 class House extends Model {
-    
+   
+    public function add_House(  $contract, $title, $address, $description, $area, $charge, $rooms, $epd, 
+                                $kitchen, $parking, $exterior, $pool, $landArea, $floor, $outbuilding, $price) {
+        $sql = "INSERT INTO property (contract, title, address, description, area, charge, rooms, epd, kitchen, parking, exterior, price)
+                VALUES ('$contract', '$title', '$address', '$description', '$area', '$charge', '$rooms', '$epd', '$kitchen', '$parking', '$exterior', '$price');
+
+                INSERT INTO house (id_property, pool, landArea, floor, outbuilding)
+                VALUES (LAST_INSERT_ID(), '$pool', '$landArea', '$floor', '$outbuilding');";
+        $this->executeRequest($sql);
+    }
+
 }
