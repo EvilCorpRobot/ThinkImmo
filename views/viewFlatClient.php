bcba60a42da99bcba2e7c4703f30a6afd8a62bc8
Vianney <vianney.giovannelli.simplon@gmail.com>
2023-03-23 08:55:02 +0100

    add works on flat

--- views/viewFlatClient.php
+++ views/viewFlatClient.php
@@ -5,19 +5,81 @@ $this->titre = "Flat";
 ?>
 
 <link rel="stylesheet" href="./views/styles/descriptionflat.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css" />
 
-<h1>Location</h1>
-<span id="type_location">Type de bien</span>
-<div class="image_flat_content">
-    <div class="col-1">
-        <img src="https://placehold.it/480x430" alt="Image 1">
+<div class="description_container">
+    <h2 class="location">Location</h2>
+    <span id="type_location">Type de bien</span>
+    <div class="image_flat_content">
+        <div class="colo-1">
+            <img src="https://placehold.it/480x430" alt="Image 1">
+        </div>
+        <div class="colo-2">
+            <img src="https://placehold.it/480x215" alt="Image 2">
+            <img src="https://placehold.it/480x215" alt="Image 3">
+        </div>
     </div>
-    <div class="col-2">
-        <img src="https://placehold.it/480x215" alt="Image 2">
-        <img src="https://placehold.it/480x215" alt="Image 3">
+    <p id="address_location">Adresse</p>
+    <p id="price_location">Prix</p>
+    <p id="description">Description</p>
+    <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, nihil dolore suscipit omnis excepturi vitae rem enim est, necessitatibus natus quas asperiores sed officia exercitationem, voluptate nostrum nemo aliquam deleniti sint quisquam velit blanditiis recusandae hic? Atque ad tempora corrupti doloremque laborum reprehenderit consectetur sapiente ipsum rerum beatae. Debitis tenetur nisi ut doloremque, cupiditate fugit expedita culpa repellat eius quia facere aliquam quo saepe dolor qui voluptas voluptate recusandae nobis, tempore dicta, perferendis blanditiis. Impedit optio excepturi consequatur minima, possimus ullam? Nemo quidem deleniti in culpa est, hic eveniet earum ipsam cupiditate tempore. Cumque magni facilis corporis explicabo in. Odio maxime numquam soluta ea quia rem harum ducimus, eligendi ipsa illum debitis iure beatae modi aperiam deleniti! Nihil laudantium ullam voluptates consequuntur ipsa, molestias assumenda eius eligendi tempora excepturi facilis aut harum quia dolores commodi rerum dolor totam, illo itaque iure eaque esse aliquid hic. Totam aliquam saepe blanditiis recusandae!</span>
+    <div class="critere">
+
+        <table>
+            <thead>
+                <tr>
+                    <th colspan="6">Critères</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td rowspan="2"><img src="icone.png" alt="Icône"></td>
+                    <td>Surface</td>
+                    <td rowspan="2"><img src="icone.png" alt="Icône"></td>
+
+                    <td>Charge/mois</td>
+                    <td rowspan="2"><img src="icone.png" alt="Icône"></td>
+
+                    <td>Nombre de pièces</td>
+                </tr>
+                <tr>
+                    <td>$m²</td>
+                    <td>$€</td>
+                    <td>$piece</td>
+                </tr>
+                <tr>
+                    <td rowspan="2"><img src="icone.png" alt="Icône"></td>
+
+                    <td>Note environnementale</td>
+                    <td rowspan="2"><img src="icone.png" alt="Icône"></td>
+
+                    <td>Cuisine</td>
+                    <td rowspan="2"><img src="icone.png" alt="Icône"></td>
+
+                    <td>Parking</td>
+                </tr>
+                <tr>
+                    <td>$note</td>
+                    <td>$gaz</td>
+                    <td>$oui/non</td>
+                </tr>
+                <tr>
+                    <td rowspan="2"><img src="icone.png" alt="Icône"></td>
+
+                    <td>Extérieur</td>
+                    <td rowspan="2"><img src="icone.png" alt="Icône"></td>
+
+                    <td>Parcel m²</td>
+                    <td rowspan="2"><img src="icone.png" alt="Icône"></td>
+
+                    <td>Numéro d'étage</td>
+                </tr>
+                <tr>
+                    <td>$jardin</td>
+                    <td>$parm²</td>
+                    <td>$étages</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </div>
-<span id="address_location">Adresse</span>
-<span id="price_location">Prix</span>
-
-<input type="radio">
