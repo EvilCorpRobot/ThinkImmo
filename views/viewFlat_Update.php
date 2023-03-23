61212201741c046968a1c7bec2730b08c7955898
Thibaud-M326 <thibaudmaitre@gmail.com>
2023-03-23 18:49:32 +0100

    update House working

--- views/viewFlat_Update.php
+++ views/viewFlat_Update.php
@@ -4,69 +4,60 @@ $this->titre = "Modification Appartement";
 
 ?>
 
-<form action="#">
-    <label for="contrat">Contrat</label><br>
-    <input type="radio" name="location" value="location">
+<form action="./index.php?action=updateFlat" method="post">
+
+    
+    <label for="contract">Contrat</label><br>
+    <input type="radio" name="contract" value="location">
     <label for="location">Location</label>
-    <input type="radio" name="achat" value="achat">
+    <input type="radio" name="contract" value="achat">
     <label for="achat">Achat</label><br>
 
-    <label for="titre">Titre</label><br>
-    <input type="text"><br>
+    <label for="title">Titre</label><br>
+    <input type="text" name="title"><br>
 
-    <label for="Adresse">Adresse</label><br>
-    <input type="text"><br>
+    <!-- <img src="https://placehold.it/480x430" alt="Image 1">
+    <img src="https://placehold.it/480x430" alt="Image 1">
+    <img src="https://placehold.it/480x430" alt="Image 1"> -->
+
+
+    <label for="address">Adresse</label><br>
+    <input type="text" name="address"><br>
     
-    <label for="Description">Description</label><br>
-    <textarea></textarea><br>
+    <label for="description">Description</label><br>
+    <textarea name="description"></textarea><br>
     <!-- --- -->
-    <label for="Type">Type</label><br>
-    <input type="radio" name="maison" value="maison">
-    <label for="maison">Maison</label>
-    <input type="radio" name="appartement" value="appartement">
-    <label for="appartement">Appartement</label><br>
+    <label for="area">Surface m2</label><br>
+    <input type="number" name="area"><br>
 
-    <label for="surface">Surface m2</label><br>
-    <input type="text"><br>
+    <label for="charge">Charge/mois</label><br>
+    <input type="number" name="charge"><br>
 
-    <label for="surface">Charge/mois</label><br>
-    <input type="text"><br>
+    <label for="rooms">Nombre de pieces</label><br>
+    <input type="number" name="rooms"><br>
     <!-- --- -->
-    <label for="nbPiece">Nombre de pieces</label><br>
-    <input type="text"><br>
-
     <label for="epd">Note environementale</label><br>
-    <input type="text"><br>
+    <input type="text" name="epd"><br>
 
     <label for="kitchen">Cuisine</label><br>
-    <input type="text"><br>
-    <!-- --- -->
+    <input type="text" name="kitchen"><br>
+
     <label for="parking">Parking</label><br>
-    <input type="text"><br>
-
+    <input type="number" name="parking"><br>
+    <!-- --- -->
     <label for="exterior">Exterieur</label><br>
-    <input type="text"><br>
+    <input type="text" name="exterior"><br>
 
-    <label for="pool">Piscine</label><br>
-    <input type="radio" name="pool" value="oui">
-    <label for="pool">Oui</label>
-    <input type="radio" name="pool" value="non">
-    <label for="pool">Non</label><br>
+    <label for="parcel">Parcelle m2</label><br>
+    <input type="number" name="parcel"><br>
+
+    <label for="floorNumber">Numero d'Etage</label><br>
+    <input type="number" name="floorNumber"><br>
     <!-- --- -->
-    <label for="landArea">Terrain m2</label><br>
-    <input type="text"><br>
+    <label for="price">Prix en €</label><br>
+    <input type="number" name="price"><br>
 
-    <label for="floor">Etage</label><br>
-    <input type="text"><br>
-
-    <label for="outbuilding">Dependance</label><br>
-    <input type="radio" name="outbuilding" value="oui">
-    <label for="outbuilding">Oui</label>
-    <input type="radio" name="outbuilding" value="non">
-    <label for="outbuilding">Non</label><br>
-    <!-- --- -->
-    <label for="price">Price</label><br>
-    <input type="text"><br>
+    <input type="hidden" name="type" value="Appartement">
 
     <input type="submit" value="Creation">
 </form>
