3f648182cf37f0d44ac97a3c90bab00288715e0b
Vianney <vianney.giovannelli.simplon@gmail.com>
2023-03-24 09:17:40 +0100

    add css

--- views/viewFlatClient.php
+++ views/viewFlatClient.php
@@ -7,80 +7,83 @@ $this->titre = "Flat";
 <link rel="stylesheet" href="./views/styles/descriptionflat.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css" />
 
-<div class="description_container">
-    <h2 class="location">Location</h2>
-    <span id="type_location">Type de bien</span>
-    <div class="image_flat_content">
-        <div class="colo-1">
-            <img src="https://placehold.it/480x430" alt="Image 1">
-        </div>
-        <div class="colo-2">
-            <img src="https://placehold.it/480x215" alt="Image 2">
-            <img src="https://placehold.it/480x215" alt="Image 3">
-        </div>
-    </div>
-    <p id="address_location">Adresse</p>
-    <p id="price_location">Prix</p>
-    <p id="description">Description</p>
-    <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, nihil dolore suscipit omnis excepturi vitae rem enim est, necessitatibus natus quas asperiores sed officia exercitationem, voluptate nostrum nemo aliquam deleniti sint quisquam velit blanditiis recusandae hic? Atque ad tempora corrupti doloremque laborum reprehenderit consectetur sapiente ipsum rerum beatae. Debitis tenetur nisi ut doloremque, cupiditate fugit expedita culpa repellat eius quia facere aliquam quo saepe dolor qui voluptas voluptate recusandae nobis, tempore dicta, perferendis blanditiis. Impedit optio excepturi consequatur minima, possimus ullam? Nemo quidem deleniti in culpa est, hic eveniet earum ipsam cupiditate tempore. Cumque magni facilis corporis explicabo in. Odio maxime numquam soluta ea quia rem harum ducimus, eligendi ipsa illum debitis iure beatae modi aperiam deleniti! Nihil laudantium ullam voluptates consequuntur ipsa, molestias assumenda eius eligendi tempora excepturi facilis aut harum quia dolores commodi rerum dolor totam, illo itaque iure eaque esse aliquid hic. Totam aliquam saepe blanditiis recusandae!</span>
-    <div class="critere">
-        <div class="flat_table">
-            <table>
-                <thead>
-                    <tr>
-                        <th colspan="6">Critères</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td rowspan="2"><img src="icone.png" alt="Icône"></td>
-                        <td>Surface</td>
-                        <td rowspan="2"><img src="icone.png" alt="Icône"></td>
+<div class="flat_create_container">
+    <div class="form_flat_create">
+        <form action="./index.php?action=addFlat" method="post">
+            <div class="flat_area">
+                <label for="contract">Location</label><br>
 
-                        <td>Charge/mois</td>
-                        <td rowspan="2"><img src="icone.png" alt="Icône"></td>
+                <label for="title">Titre</label><br>
 
-                        <td>Nombre de pièces</td>
-                    </tr>
-                    <tr>
-                        <td>$m²</td>
-                        <td>$€</td>
-                        <td>$piece</td>
-                    </tr>
-                    <tr>
-                        <td rowspan="2"><img src="icone.png" alt="Icône"></td>
+<div>>
+                <img src="https://placehold.it/480x430" alt="Image 1">
+                <img src="https://placehold.it/480x430" alt="Image 1">
+                <img src="https://placehold.it/480x430" alt="Image 1">
+</div>
 
-                        <td>Note environnementale</td>
-                        <td rowspan="2"><img src="icone.png" alt="Icône"></td>
+                <label for="address">Adresse</label><br>
 
-                        <td>Cuisine</td>
-                        <td rowspan="2"><img src="icone.png" alt="Icône"></td>
 
-                        <td>Parking</td>
-                    </tr>
-                    <tr>
-                        <td>$note</td>
-                        <td>$gaz</td>
-                        <td>$oui/non</td>
-                    </tr>
-                    <tr>
-                        <td rowspan="2"><img src="icone.png" alt="Icône"></td>
+                <label for="description">Description</label><br>
+                <textarea name="description"></textarea><br>
+            </div>
+            <!-- --- -->
+            <div class="flat_area_1">
+                <div>
+                    <label for="area">Surface m2</label>
+                    <input type="number" name="area">
+                </div>
+                <div>
+                    <label for="charge">Charge/mois</label>
+                    <input type="number" name="charge">
+                </div>
+                <div>
+                    <label for="rooms">Nombre de pieces</label>
+                    <input type="number" name="rooms">
+                </div>
+            </div>
+            <!-- --- -->
+            <div class="flat_area_1">
+                <div>
+                    <label for="epd">Note environementale</label>
+                    <input type="text" name="epd">
+                </div>
+                <div>
 
-                        <td>Extérieur</td>
-                        <td rowspan="2"><img src="icone.png" alt="Icône"></td>
+                    <label for="kitchen">Cuisine</label>
+                    <input type="text" name="kitchen">
+                </div>
+                <div>
 
-                        <td>Parcel m²</td>
-                        <td rowspan="2"><img src="icone.png" alt="Icône"></td>
+                    <label for="parking">Parking</label>
+                    <input type="number" name="parking">
+                </div>
+            </div>
 
-                        <td>Numéro d'étage</td>
-                    </tr>
-                    <tr>
-                        <td>$jardin</td>
-                        <td>$parm²</td>
-                        <td>$étages</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+            <!-- --- -->
+            <div class="flat_area_1">
+                <div>
+                    <label for="exterior">Exterieur</label>
+                    <input type="text" name="exterior">
+                </div>
+                <div>
+                    <label for="parcel">Parcelle m2</label>
+                    <input type="number" name="parcel">
+                </div>
+                <div>
+                    <label for="floorNumber">Numero d'Etage</label>
+                    <input type="number" name="floorNumber">
+                </div>
+            </div>
+            <!-- --- -->
+            <div class="flat_area_1">
+
+                <div>
+                    <label for="price">Prix en €</label>
+                    <input type="number" name="price">
+                </div>
+
+            </div>
+        </form>
     </div>
 </div>
--- views/viewFlat_Create.php
+++ views/viewFlat_Create.php
@@ -1,61 +1,95 @@
-<?php 
+<?php
 
 $this->titre = "Creation Appartement";
 
 ?>
 
-<form action="./index.php?action=addFlat" method="post">
-    <label for="contract">Contrat</label><br>
-    <input type="radio" name="contract" value="location">
-    <label for="location">Location</label>
-    <input type="radio" name="contract" value="achat">
-    <label for="achat">Achat</label><br>
+<link rel="stylesheet" href="./views/styles/flatcreate.css">
+<div class="flat_create_container">
+    <div class="form_flat_create">
+        <form action="./index.php?action=addFlat" method="post">
+            <div class="flat_area">
+                <label for="contract">Contrat</label><br>
+                <input type="radio" name="contract" value="location">
+                <label for="location">Location</label>
+                <input type="radio" name="contract" value="achat">
+                <label for="achat">Achat</label><br>
 
-    <label for="title">Titre</label><br>
-    <input type="text" name="title"><br>
+                <label for="title">Titre</label><br>
+                <input type="text" name="title"><br>
 
-    <!-- <img src="https://placehold.it/480x430" alt="Image 1">
+                <!-- <img src="https://placehold.it/480x430" alt="Image 1">
     <img src="https://placehold.it/480x430" alt="Image 1">
     <img src="https://placehold.it/480x430" alt="Image 1"> -->
 
 
-    <label for="address">Adresse</label><br>
-    <input type="text" name="address"><br>
-    
-    <label for="description">Description</label><br>
-    <textarea name="description"></textarea><br>
-    <!-- --- -->
-    <label for="area">Surface m2</label><br>
-    <input type="number" name="area"><br>
+                <label for="address">Adresse</label><br>
+                <input type="text" name="address"><br>
 
-    <label for="charge">Charge/mois</label><br>
-    <input type="number" name="charge"><br>
+                <label for="description">Description</label><br>
+                <textarea name="description"></textarea><br>
+            </div>
+            <!-- --- -->
+            <div class="flat_area_1">
+                <div>
+                    <label for="area">Surface m2</label>
+                    <input type="number" name="area">
+                </div>
+                <div>
+                    <label for="charge">Charge/mois</label>
+                    <input type="number" name="charge">
+                </div>
+                <div>
+                    <label for="rooms">Nombre de pieces</label>
+                    <input type="number" name="rooms">
+                </div>
+            </div>
+            <!-- --- -->
+            <div class="flat_area_1">
+                <div>
+                    <label for="epd">Note environementale</label>
+                    <input type="text" name="epd">
+                </div>
+                <div>
 
-    <label for="rooms">Nombre de pieces</label><br>
-    <input type="number" name="rooms"><br>
-    <!-- --- -->
-    <label for="epd">Note environementale</label><br>
-    <input type="text" name="epd"><br>
+                    <label for="kitchen">Cuisine</label>
+                    <input type="text" name="kitchen">
+                </div>
+                <div>
 
-    <label for="kitchen">Cuisine</label><br>
-    <input type="text" name="kitchen"><br>
+                    <label for="parking">Parking</label>
+                    <input type="number" name="parking">
+                </div>
+            </div>
 
-    <label for="parking">Parking</label><br>
-    <input type="number" name="parking"><br>
-    <!-- --- -->
-    <label for="exterior">Exterieur</label><br>
-    <input type="text" name="exterior"><br>
-
-    <label for="parcel">Parcelle m2</label><br>
-    <input type="number" name="parcel"><br>
-
-    <label for="floorNumber">Numero d'Etage</label><br>
-    <input type="number" name="floorNumber"><br>
-    <!-- --- -->
-    <label for="price">Prix en €</label><br>
-    <input type="number" name="price"><br>
-
-    <input type="hidden" name="type" value="Appartement">
-
-    <input type="submit" value="Creation">
-</form>
+            <!-- --- -->
+            <div class="flat_area_1">
+                <div>
+                    <label for="exterior">Exterieur</label>
+                    <input type="text" name="exterior">
+                </div>
+                <div>
+                    <label for="parcel">Parcelle m2</label>
+                    <input type="number" name="parcel">
+                </div>
+                <div>
+                    <label for="floorNumber">Numero d'Etage</label>
+                    <input type="number" name="floorNumber">
+                </div>
+            </div>
+            <!-- --- -->
+            <div class="flat_area_1">
+                <div>
+                    <label for="price">Prix en €</label>
+                    <input type="number" name="price">
+                </div>
+                <div>
+                    <input type="hidden" name="type" value="Appartement">
+                </div>
+                <div>
+                    <input type="submit" value="Creation" class="create_flat_btn">
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
