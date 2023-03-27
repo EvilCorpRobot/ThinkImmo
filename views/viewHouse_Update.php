5863225437326a7b368c8cccc358117b95fb3dfa
Vianney <vianney.giovannelli.simplon@gmail.com>
2023-03-27 12:20:00 +0200

    add style update

--- views/viewHouse_Update.php
+++ views/viewHouse_Update.php
@@ -1,101 +1,138 @@
-<?php 
-
-    $this->titre = "Mofification Maison";
-
-?> 
-
 <?php
 
-    $idProperty = $_GET['id_property'];
-
-    // echo $idProperty;
+$this->titre = "Mofification Maison";
 
 ?>
 
-<form action="./index.php?action=updateHouse&id_property=<?= $idProperty ?>" method="post">
+<?php
 
-    <?php foreach($houseInfo as $info) { ?>
+$idProperty = $_GET['id_property'];
 
-        <?php if($info['contract'] == "location") { ?>
-            <label for="contract">Contrat</label><br>
-            <input type="radio" name="contract" value="location" checked>
-            <label for="location">Location</label>
-            <input type="radio" name="contract" value="achat">
-            <label for="achat">Achat</label><br>
-        <?php } else { ?>
-            <label for="contract">Contrat</label><br>
-            <input type="radio" name="contract" value="location">
-            <label for="location">Location</label>
-            <input type="radio" name="contract" value="achat" checked>
-            <label for="achat">Achat</label><br>
-        <?php } ?>
+// echo $idProperty;
 
-        <label for="title">Titre</label><br>
-        <input type="text" name="title" value="<?= $info['title'] ?>"><br>
+?>
+<link rel="stylesheet" href="./views/styles/houseUpdate.css">
 
-        <!-- <input type="file" name="picture" multiple><br> -->
-        <!-- <img src="https://placehold.it/480x430" alt="Image 1">
+<div class="form_house_update">
+    <form action="./index.php?action=updateHouse&id_property=<?= $idProperty ?>" method="post">
+
+        <?php foreach ($houseInfo as $info) { ?>
+
+            <?php if ($info['contract'] == "location") { ?>
+                <label for="contract">Contrat</label><br>
+                <input type="radio" name="contract" value="location" checked>
+                <label for="location">Location</label>
+                <input type="radio" name="contract" value="achat">
+                <label for="achat">Achat</label><br>
+            <?php } else { ?>
+                <label for="contract">Contrat</label><br>
+                <input type="radio" name="contract" value="location">
+                <label for="location">Location</label>
+                <input type="radio" name="contract" value="achat" checked>
+                <label for="achat">Achat</label><br>
+            <?php } ?>
+
+            <label for="title">Titre</label><br>
+            <input type="text" name="title" id="title" value="<?= $info['title'] ?>"><br>
+
+            <!-- <input type="file" name="picture" multiple><br> -->
+            <!-- <img src="https://placehold.it/480x430" alt="Image 1">
         <img src="https://placehold.it/480x430" alt="Image 1">
         <img src="https://placehold.it/480x430" alt="Image 1"><br> -->
 
-        <label for="address">Adresse</label><br>
-        <input type="text" name="address" value="<?= $info['address'] ?>"><br>
-        
-        <label for="description">Description</label><br>
-        <textarea name="description"><?= $info['description'] ?></textarea><br>
-        <!-- --- -->
-        <label for="area">Surface m2</label><br>
-        <input type="number" name="area" value="<?= $info['area'] ?>"><br>
+            <label for="address">Adresse</label><br>
+            <input type="text" name="address" value="<?= $info['address'] ?>"><br>
 
-        <label for="charge">Charge/mois</label><br>
-        <input type="number" name="charge" value="<?= $info['charge'] ?>"><br>
+            <label for="description">Description</label><br>
+            <textarea name="description" class="update_description"><?= $info['description'] ?></textarea><br>
+            <!-- --- -->
+            <div class="house_area_1">
+                <div>
+                    <label for="area">Surface m2</label>
+                    <input type="number" name="area" value="<?= $info['area'] ?>">
+                </div>
+                <div>
+                    <label for="charge">Charge/mois</label>
+                    <input type="number" name="charge" value="<?= $info['charge'] ?>">
+                </div>
+                <div>
+                    <label for="rooms">Nombre de pieces</label>
+                    <input type="number" name="rooms" value="<?= $info['rooms'] ?>">
+                </div>
+            </div>
+            <!-- --- -->
+            <div class="house_area_2">
+                <div>
+                    <label for="epd">Note environementale</label>
+                    <input type="text" name="epd" value="<?= $info['epd'] ?>">
+                </div>
+                <div>
+                    <label for="kitchen">Cuisine</label>
+                    <input type="text" name="kitchen" value="<?= $info['kitchen'] ?>">
+                </div>
+                <div>
+                    <label for="parking">Parking</label>
+                    <input type="number" name="parking" value="<?= $info['parking'] ?>">
+                </div>
+            </div>
+            <!-- --- -->
+            <div class="house_area_3">
+                <div>
+                    <label for="exterior">Exterieur</label>
+                    <input type="text" name="exterior" value="<?= $info['exterior'] ?>">
+                </div>
+                <div>
+                    <?php if ($info['pool'] == "oui") { ?>
+                        <label for="pool">Piscine</label>
+                        <div class="form-check-inline">
+                            <input type="radio" name="pool" value="oui" checked>
+                            <label for="oui">Oui</label>
+                            <input type="radio" name="pool" value="non">
+                            <label for="non">Non</label>
+                        </div>
+                    <?php } else { ?>
+                        <label for="pool">Piscine</label>
+                        <div class="form-check-inline">
+                            <input type="radio" name="pool" value="oui">
+                            <label for="oui">Oui</label>
+                            <input type="radio" name="pool" value="non" checked>
+                            <label for="non">Non</label>
+                        </div>
+                    <?php } ?>
+                </div>
+                <div>
+                    <label for="landArea">Terrain m2</label>
+                    <input type="number" name="landArea" value="<?= $info['landArea'] ?>">
+                </div>
+            </div>
+            <!-- --- -->
+            <div class="house_area_4">
+                <div>
+                    <label for="floor">Etage</label>
+                    <input type="number" name="floor" value="<?= $info['floor'] ?>">
+                </div>
+                <div>
+                    <label for="outbuilding">Dependance</label>
+                    <input type="number" name="outbuilding" value="<?= $info['outbuilding'] ?>">
+                </div>
+                <div>
+                    <label for="price">Price</label>
+                    <input type="number" name="price" value="<?= $info['price'] ?>">
+                </div>
+            </div>
+            <!-- --- -->
+            <div class="house_area_5">
 
-        <label for="rooms">Nombre de pieces</label><br>
-        <input type="number" name="rooms" value="<?= $info['rooms'] ?>"><br>
-        <!-- --- -->
-        <label for="epd">Note environementale</label><br>
-        <input type="text" name="epd" value="<?= $info['epd'] ?>"><br>
-
-        <label for="kitchen">Cuisine</label><br>
-        <input type="text" name="kitchen" value="<?= $info['kitchen'] ?>"><br>
-
-        <label for="parking">Parking</label><br>
-        <input type="number" name="parking" value="<?= $info['parking'] ?>"><br>
-        <!-- --- -->
-        <label for="exterior">Exterieur</label><br>
-        <input type="text" name="exterior" value="<?= $info['exterior'] ?>"><br>
-
-        <?php if($info['pool'] == "oui") { ?>
-            <label for="pool">Piscine</label><br>
-            <input type="radio" name="pool" value="oui" checked>
-            <label for="oui">Oui</label>
-            <input type="radio" name="pool" value="non">
-            <label for="non">Non</label><br>
-        <?php } else { ?>
-            <label for="pool">Piscine</label><br>
-            <input type="radio" name="pool" value="oui">
-            <label for="oui">Oui</label>
-            <input type="radio" name="pool" value="non" checked>
-            <label for="non">Non</label><br>
-        <?php } ?>
-
-        <label for="landArea">Terrain m2</label><br>
-        <input type="number" name="landArea" value="<?= $info['landArea'] ?>"><br>
-        <!-- --- -->
-        <label for="floor">Etage</label><br>
-        <input type="number" name="floor" value="<?= $info['floor'] ?>"><br>
-
-        <label for="outbuilding">Dependance</label><br>
-        <input type="number" name="outbuilding" value="<?= $info['outbuilding'] ?>"><br>
-        <!-- --- -->
-        <label for="price">Price</label><br>
-        <input type="number" name="price" value="<?= $info['price'] ?>"><br>
-
-        <input type="hidden" name="type" value="Maison">
-
-        <input type="submit" value="Modifier">
-    <?php 
-        break;
-    }
-    ?>
-</form>
+                <div>
+                    <input type="hidden" name="type" value="Maison">
+                </div>
+                <div>
+                    <input type="submit" value="Modifier">
+                <?php
+                break;
+            }
+                ?>
+                </div>
+            </div>
+    </form>
+</div>
